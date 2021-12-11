fcbbcfcfe70d37fc79abaf98577e338306442b80
Priyankagupta186 <85233284+Priyankagupta186@users.noreply.github.com>
2021-12-11 12:17:33 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/screens/home_page.dart';
+import 'package:flutter_catalog/screens/login_page.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -12,7 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
    
     return MaterialApp(
-      home: HomePage(),
+     // home: HomePage(),
+      routes: {
+        '/':(context)=>HomePage(),
+        '/login':(context)=>LoginPage(),
+      },
     );
   }
 }
